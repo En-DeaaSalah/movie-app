51349147a6d832b7e680626960da37f2f07a30c2
DiaaSalah <diaa.salah@miroworld.com>
2023-12-19 14:20:31 +0300

    create routes folder and pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RouterProvider} from "react-router";
+import {router} from "./routes";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(document.getElementById("root") as any).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
